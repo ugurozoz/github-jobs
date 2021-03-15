d10c0e354ef62f7b5e87208799c2d08c69c6154f
ugur ozoz <ugurozoz@yahoo.com>
2021-03-15 21:29:42 +0300

    readme changed

---
+++ src/portfolio.js
@@ -0,0 +1,14 @@
+const ugur = {
+  Uğur Özöz: {
+    about: "Frontend Developer who has Bachelor's degree in physics lives in istanbul",      
+    skills: ["JS", "CSS","SASS", "HTML", "D3JS","REACT","REDUX", "THREEJS", "GIT"]
+    currentEmployer: "Milliyet",
+    projects: {
+      atWork: ["milliyet.com.tr","Skorer", "Milliyet Cadde", "Pembenar", "Uzmanpara",...etc] ,
+      self: [ "http://zehrakaratas.art/", "http://githubjobs.uurz.net/"]
+    }
+
+      
+    
+  } 
+}
